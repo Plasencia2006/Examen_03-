4eb532c46a12298c308b10e8efee4a1d064cb5f2
Plasencia2006 <plasenciaalvahectorisai@gmail.com>
2025-11-04 08:20:09 -0500

    Elimino Procfile y configuro Railway con php -S

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/clear-cache', function () {
-    Artisan::call('optimize:clear');
-    Artisan::call('config:clear');
-    Artisan::call('cache:clear');
-    Artisan::call('view:clear');
-    Artisan::call('route:clear');
-    return '✅ Caché de Laravel limpiada correctamente.';
-});
+
