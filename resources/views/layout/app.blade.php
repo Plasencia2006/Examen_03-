e3e53e4749e1b7ccbc67cf6300ed12e8d224c26b
Plasencia2006 <plasenciaalvahectorisai@gmail.com>
2025-11-04 18:14:47 -0500

    solucion error 500

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tienda de Calzados</title>
-    <!--<link rel="stylesheet" href="{{ mix('css/app.css') }}">-->
+    @vite('resources/css/app.css')
     <style>
         /* Fondo con gradiente sutil */
         body {
